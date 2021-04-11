09625bcc99b6325b9cf6b6c5f9b41d54b1de3b44
Đinh Công Tuyền <dinhcongtuyen.@gmail.com>
2021-04-11 21:43:49 +0700

    start

---
+++ app/src/main/java/com/example/pjpongmusicplayer/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.pjpongmusicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = (TextView)findViewById(R.id.textView2);
+        Animation mAnimation = new AlphaAnimation(1, 0);
+        mAnimation.setDuration(200);
+        mAnimation.setRepeatCount(Animation.INFINITE);
+        mAnimation.setRepeatMode(Animation.REVERSE); tv.startAnimation(mAnimation);
+
+        ImageView mainImage = (ImageView) findViewById(R.id.imageView);
+        mainImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent layoutLogin = new Intent(MainActivity.this,login.class);
+                startActivity(layoutLogin);
+            }
+        });
+    }
+}
