d3a06ba21ed1572a1d64d55fceeb290109c0f6a9
Đinh Công Tuyền <dinhcongtuyen.@gmail.com>
2021-04-11 23:34:01 +0700

    start

--- app/src/main/java/com/example/pjpongmusicplayer/login.java
+++ app/src/main/java/com/example/pjpongmusicplayer/login.java
@@ -2,7 +2,12 @@ package com.example.pjpongmusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
