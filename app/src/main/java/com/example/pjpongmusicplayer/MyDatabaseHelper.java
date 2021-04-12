b9ff914d49b78a9b6bf7eaaafef4774c73558354
DESKTOP-SA24TAL\dinhc <dinhcongtuyen.@gmail.com>
2021-04-12 18:31:46 +0700

    start

--- app/src/main/java/com/example/pjpongmusicplayer/MyDatabaseHelper.java
+++ app/src/main/java/com/example/pjpongmusicplayer/MyDatabaseHelper.java
@@ -11,26 +11,26 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 // ...
     private static final String DATABASE_NAME = "Account_Manager";
     private static final int DATABASE_VERSION = 1;
-    private static final String TABLE_ACCOUNT = "Accout";
-    private static final String COLUMN_ACCOUNT_ID = "Account_Id";
-    private static final String COLUMN_ACCOUNT_NAME = "Account_Name";
-    private static final String COLUMN_ACCOUNT_PASSWORD = "Account_Password";
-    private static final String COLUMN_ACCOUNT_USER_NAME = "Account_User_Name";
+    private static final String TABLE_USER = "TB_User";
+    private static final String COLUMN_USER_ID = "User_Id";
+    private static final String COLUMN_USER_NAME = "User_Name";
+    private static final String COLUMN_USER_PHONE = "User_Phone_Number";
+    private static final String COLUMN_USER_BIRTHDAY = "User_Birthday";
+    private static final String COLUMN_USER_ACCOUNT = "User_Account";
+    private static final String COLUMN_USER_PASSWORD = "User_Password";
 
     public MyDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-        // Script to create table.
-//        String script = "CREATE TABLE " + TABLE_ACCOUNT + "("
-//                + COLUMN_NOTE_ID + " INTEGER PRIMARY KEY," + COLUMN_NOTE_TITLE + " TEXT,"
-//                + COLUMN_NOTE_CONTENT + " TEXT" + ")";
-        // Execute script.
-//        db.execSQL(script);
+        //Script to create table.
+        String script = "CREATE TABLE " + TABLE_USER + "("
+                + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_USER_NAME + " TEXT,"
+                + COLUMN_USER_PHONE + " TEXT," + COLUMN_USER_BIRTHDAY+"INT,"+ COLUMN_USER_ACCOUNT+" TEXT,"+COLUMN_USER_PASSWORD+ " TEXT)";
+        //Execute script.
+        db.execSQL(script);
     }
-
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
